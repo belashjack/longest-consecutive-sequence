4367d9c27c2880d73b26c4e8e0215423fa268559
Yauheni Belash <belashjack1@gmail.com>
2018-03-13 12:46:46 +0300

    Create function that computes longest consecutive sequence length

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+	// your solution here
+	let hash = {}, maxLength = 0;
+
+	for (let i = 0; i < array.length; i++) {
+		hash[array[i]] = array[i];
+	}
+
+	for (const key in hash) {
+		if (hash[+key - 1] === undefined) {
+			let j = 0;
+			while (hash[+key + j] !== undefined) {
+				j++;
+			}
+			maxLength = Math.max(maxLength, j);
+		}
+	}
+
+	return maxLength;
 }
